ae6a2fbd5601ebba5448960647d41d1518c5dab8
Johannes Cartus <cartus@student.tugraz.at>
2019-05-07 15:37:48 +0200

    ADD: initial commit, problem 1

---
+++ export_aims_metadata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# export directory of aimes executable
+export MY_AIMS_DIR=$HOME/Software/fhi-aims.171221_1
+
+# export version
+export MY_AIMS_VERSION=171221_1
+
+# export alias
+export fhi_aims=$MY_AIMS_DIR/bin/aims.$MY_AIMS_VERSION.serial.x
